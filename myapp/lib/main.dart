3a1fc0a80c55da7d9fa1e149435641effecdac06
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-10-11 22:09:41 -0300

    Organizando pastas e arquivos

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,38 +1,6 @@
 import 'package:flutter/material.dart';
+import 'my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      primarySwatch: Colors.pink,
-    ),
-    home: const HomePage(),
-  );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Meu App"),
-      ),
-    );
-  }
 }
