43ee3e04af15cfd64c1fb0d438ed3e90eaee722d
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-10-11 22:20:42 -0300

    Criando arquivo para calcular IMC

--- myapp/lib/service/calculate_imc.dart
+++ myapp/lib/service/calculate_imc.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 class CalculateIMCService extends StatefulWidget {
-  
+
+  @override
+  State<CalculateIMCService> createState() => _CalculateIMCState();
+}
+
+class _CalculateIMCState extends State<CalculateIMCService> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
